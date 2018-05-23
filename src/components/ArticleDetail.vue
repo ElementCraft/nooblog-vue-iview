01216edcd78248467905c3d1390063ef6595a3bb
小王子 <976228284@qq.com>
2018-05-24 07:22:39 +0800

    fix

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -3,13 +3,13 @@
 		<div class="article-header">{{articleInfo.title}}</div>
 		<div class="article-info">
 			<Row>
-				<Col :lg="6" :md="6" :sm="6" :xs="8">
+				<Col :lg="6" :md="6" :sm="6" :xs="10">
 					<router-link to="/">作者: {{articleInfo.userColumn.user.nickName}}</router-link>
 				</Col>
-				<Col :lg="5" :md="5" :sm="5" :xs="6">栏目: {{articleInfo.userColumn.title}}</Col>
+				<Col :lg="5" :md="5" :sm="5" :xs="14">栏目: {{articleInfo.userColumn.title}}</Col>
 				<Col :lg="4" :md="4" :sm="4" :xs="0">发表时间: {{articleInfo.gmtCreate}}</Col>
-				<Col :lg="9" :md="9" :sm="9" :xs="10">
-					标签: {{articleInfo.label}}
+				<Col :lg="9" :md="9" :sm="9" :xs="0">
+					标签: <Tag v-for="label in articleInfo.label" v-if="label.trim() != ''" :key="label" color="lightblue">{{label}}</Tag> 
 				</Col>
 			</Row>
 		</div>
@@ -44,7 +44,7 @@ export default {
 			let result = res.body;
 
 			result.gmtCreate = CountAgo(result.gmtCreate);
-			//result.label = result.label.replace(";;", " ");
+			result.label = result.label.split(";;");
 
 			this.articleInfo = result;
 
@@ -80,10 +80,15 @@ export default {
 	border-bottom: 1px solid lightgray;
 	padding-bottom: 0px;
 	line-height: 42px;
+	font-size: 12px;
 	text-align: center;
 }
 
 .article-symbol{
 	text-align: center;
+}
+
+.ivu-tag{
+	margin-bottom: 4px;
 }
 </style>
